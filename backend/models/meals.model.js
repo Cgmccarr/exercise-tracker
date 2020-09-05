e2f5ed696aa0c1c306834d9daaa23ed50babb3c0
cgmccarr <cgmccarr@gmail.com>
2020-09-04 23:26:21 -0400

    added meals schema

---
+++ backend/models/meals.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const mealsSchema = new Schema({
+    food: {type: String, required: true},
+    carbs: {type: Number, required: true},
+    protein: {type: Number, required: true},
+    carbs: {type: Number, required: true},
+    fiber: {type: Number, required: false},
+}, {
+    timestamps: true
+});
+
+const Meals = mongoose.model('Meals', mealsSchema);
+
+module.exports = Meals;
