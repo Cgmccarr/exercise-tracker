831718e0245fbcc1b16ddbe839f8c0821d7c6676
cgmccarr <cgmccarr@gmail.com>
2020-09-08 11:45:36 -0400

    Added create meal component

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css"
 import axios from 'axios';
-//import { response } from 'express';
-
 
 export default class CreateExercises extends Component {
 constructor(props) {
